c66048b206a12e58675a28c4391f40bc3150eaac
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-04-10 07:07:13 +0300

    write logic for my datecount pipe

--- src/app/datecount.pipe.ts
+++ src/app/datecount.pipe.ts
@@ -5,8 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DatecountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let todaysdate:any=new Date();
+  
+    var dateDifference=Math.abs(value-todaysdate);
+    var dateSeconds = dateDifference*0.001;
+    const seconds = 86400;
+    var daysPassed= Math.floor(dateSeconds/seconds);
+
+    return daysPassed;
   }
 
 }
